4b3960ea184fb2c4a58d242e9def8e7d181acd2e
heldals <heldaludya5@gmail.com>
2024-02-19 12:11:23 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo aspernatur exercitationem dolores porro explicabo vel ab! Autem quibusdam corporis et maxime delectus consequatur, fugiat distinctio, dignissimos animi qui quos sint!</p>
 </body>
 </html>
