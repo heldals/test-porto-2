5f8355d41a74aa844a3970f2df03b8c5f242f94d
heldals <heldaludya5@gmail.com>
2024-02-19 12:23:01 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo aspernatur exercitationem dolores porro explicabo vel ab! Autem quibusdam corporis et maxime delectus consequatur, fugiat distinctio, dignissimos animi qui quos sint!</p>
+    <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ul>
 </body>
 </html>
